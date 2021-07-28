169a9423821c13955335a63c8fbb373e7bc1a829
selahattinkaraca <79923318+selahattinkaraca@users.noreply.github.com>
2021-07-28 09:25:24 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,72 @@
+using Lügavi.Models;
+using Lügavi.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Lügavi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddTransient<JsonWikiService>();
+            services.AddTransient<JsonProjectService>();
+            services.AddTransient<JsonKelimeService>();
+            services.AddControllers();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                //endpoints.MapGet("kelimeler",(context) => 
+                //{
+                //    var kelimeler = app.ApplicationServices.GetService<JsonKelimeService>().GetKelimeler();
+                //    var json = JsonSerializer.Serialize<IEnumerable<KelimeModel>>(kelimeler);
+                //    return context.Response.WriteAsync(json);
+                //});
+            });
+        }
+    }
+}
